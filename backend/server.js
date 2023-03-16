f8da863e2d5e57ea9eb0b968cc43bd4c6f0a0cdf
Pratik <pratikpsangani03@gmail.com>
2023-03-16 23:06:05 +0530

    hosting setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
+import path from "path";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
@@ -25,6 +26,19 @@ app.use("/api/orders", orderRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    );
+  });
+}else{
+  app.get("/",(req,res)=>{
+    res.send("You are in development mode, switch to production mode....")
+  })
+}
 
 app.use(notFound);
 
